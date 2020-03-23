3b6267603f7df6ae7a8ac925c290590c794f624e
lexkahng <lexkahng@gmail.com>
2020-03-23 11:34:04 -0700

    fixing loadtest

--- http_loadtest.sh
+++ http_loadtest.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# must be ran inline
 proxyIp=172.16.171.132
 proxyPort=8005
 
 for i in {1..10000};do
-    curl http://$proxyIp:$proxyPort >> /dev/null
+    telnet 172.16.171.132 8005 &
+    sleep 0.5
 done
