82e85751f151c198ef9091193e5f3be17eaf585d
lexkahng <lexkahng@gmail.com>
2020-03-23 17:42:16 -0700

    loadtest now reads from config file

--- ssh_loadtester.py
+++ ssh_loadtester.py
@@ -2,27 +2,14 @@
 
 import sys, os, string, threading
 import paramiko
-import json
 
 lock = threading.Lock()
 
 def main():
     threads = []
-    # host = input("Enter proxy IP: ")
-    # port = input("Enter proxy port: ")
-    # load = input("Enter number of connections: ")
-    # sleeptime = input("Enter sleeptime (seconds): ")
-    with open('loadtest.json', 'r') as f:
-        loadtest_config = json.load(f)
-    host = loadtest_config['host']
-    port = loadtest_config['port']
-    load = loadtest_config['load']
-    sleeptime = loadtest_config['sleeptime']
-
-    #host, port = '172.16.171.132', 8022
-    #run = "echo hi; sleep 10"
-    run = "echo hi; sleep " + str(sleeptime)
-    for _ in range(load):
+    host, port = '172.16.171.132', 8022
+    run = "echo hi; sleep 10"
+    for _ in range(20):
         thread = threading.Thread(target=sshRun, args=(host, port, run))
         thread.start()
         threads.append(thread)
