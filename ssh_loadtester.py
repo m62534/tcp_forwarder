369dc2445cc75d5430fda08ef72d50cf55c04943
lexkahng <lexkahng@gmail.com>
2020-03-23 17:11:42 -0700

    adding loadtest ssh

--- ssh_loadtester.py
+++ ssh_loadtester.py
@@ -8,13 +8,14 @@ lock = threading.Lock()
 def main():
     threads = []
     host, port = '172.16.171.132', 8022
-    run = "df -h; sleep 10"
-    for _ in range(1):
+    run = "echo hi; sleep 10"
+    for _ in range(100):
         thread = threading.Thread(target=sshRun, args=(host, port, run))
         thread.start()
         threads.append(thread)
     for thread in threads:
         thread.join()
+    
 
 def sshRun(host,port,run):
     ssh = paramiko.SSHClient()
@@ -23,7 +24,8 @@ def sshRun(host,port,run):
     stdin, stdout, stderr = ssh.exec_command(run)
     stdin.write('xy\n')
     stdin.flush()
-    #stdout.flush()
+    with lock:
+        _ = stdout.readlines()
 
 if __name__ == "__main__":
     main()
