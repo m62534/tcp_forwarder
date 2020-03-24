2136c83876b7b17aa37b745a12089c4cdb4afbd1
lexkahng <lexkahng@gmail.com>
2020-03-23 23:42:36 -0700

    adding view state

---
+++ view_socket_state.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+## Shows all active ESTABLISHED and TIME_WAIT connections on listening proxy port
+## USAGE: ./view_established.sh 8022 ##
+
+if [[ -z $1 ]];then
+	echo "Requires port argument"
+fi
+
+proxyIp=$(hostname -I)
+proxyPort=$1
+
+while true;do
+	echo $(date) :: $(netstat -anp | grep "$proxyIp:$proxyPort" | grep -i ESTABLISHED | wc -l) "-" "ESTABLISHED"
+	echo $(date) :: $(netstat -anp | grep "$proxyIp:$proxyPort" | grep -i TIME_WAIT | wc -l) "-" "TIME_WAIT"
+        sleep 1
+done
