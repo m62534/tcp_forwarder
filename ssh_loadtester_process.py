17030763fd6f882a5f9d3053a62b5d8416474f2b
lexkahng <lexkahng@gmail.com>
2020-03-23 18:17:11 -0700

    adding multiprocessing

--- ssh_loadtester_process.py
+++ ssh_loadtester_process.py
@@ -1,13 +1,12 @@
 #!/usr/local/bin/python3
 
-import sys, os, string, threading
+import sys, os, string
 import paramiko
 import json
-
-lock = threading.Lock()
+import multiprocessing
 
 def main():
-    threads = []
+    global host, port, run, username, password
     with open('loadtest.json', 'r') as f:
         loadtest_config = json.load(f)
     host = loadtest_config['host']
@@ -18,23 +17,23 @@ def main():
     sleeptime = loadtest_config['sleeptime']
 
     run = "echo hi; sleep " + str(sleeptime)
-    for _ in range(load):
-        thread = threading.Thread(target=sshRun, args=(host, port, run, username, password))
-        thread.start()
-        threads.append(thread)
-    for thread in threads:
-        thread.join()
+    
+    jobs = []
+    for i in range(load):
+        p = multiprocessing.Process(target=sshRun)
+        jobs.append(p)
+        p.start()
     
 
-def sshRun(host,port,run,username,password):
+def sshRun():
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host, port=port, username=username, password=password)
-    stdin, stdout, stderr = ssh.exec_command(run)
+    stdin, stdout, _ = ssh.exec_command(run)
     stdin.write('xy\n')
     stdin.flush()
-    with lock:
-        _ = stdout.readlines()
+    _ = stdout.readlines()
+    ssh.close()
 
 if __name__ == "__main__":
     main()
